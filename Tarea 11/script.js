59ff889a47d7e656d6f03f5f6cbf643379b80c9d
eduardocuapio03 <113400561+eduardocuapio03@users.noreply.github.com>
2024-10-13 22:22:13 -0600

    Add files via upload

---
+++ Tarea 11/script.js
@@ -0,0 +1,18 @@
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    }
+    
+    let a = 0;
+    let b = 1;
+    
+    for (let i = 2; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+    
+    return b;
+}
+console.log(fibonacci(10));  // Output: 55
+  
